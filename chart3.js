06cc6ab82ee89e1a442b8f7d0ecb5397154b2897
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-22 18:49:26 +0700

    chart3.js

--- chart3.js
+++ chart3.js
@@ -1,33 +1,33 @@
-const ctx3 = document.getElementById('barchart2');
-
-const barchart2 = new Chart(ctx3, {
-  type: 'bar',
-  data: {
-    labels: ['Non Carbonated', 'Carbonated', 'Food', 'Water'],
-    datasets: [{
-      label: 'Rata-Rata Transaksi',
-      data: [2.9, 2.2, 2, 2,],
-      backgroundColor: [
-          'rgba(165, 42, 42, 1)',
-          
-      ],
-      borderColor: [
-          'rgba(165, 42, 42, 1)',
-      ],
-      borderWidth: 1
-  }]
-},
-options: {
-  scales: {
-      y: {
-          beginAtZero: true,
-          ticks: {
-              stepSize: 1,
-              callback: function (value) {
-                  return value + ' ';
-              }
-          }
-      }
-  }
-}
+var ctx3 = document.getElementById('chart3').getContext('2d');
+var myChart = new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
+        datasets: [{
+            label: 'Transaction Total (Cash)',
+            data: [5210.5, 3100.75, 1845.75, 1220.5],
+            backgroundColor: '(165, 42, 42, 1)', 
+            borderColor: 'rgba(165, 42, 42, 1)', 
+            borderWidth: 1
+        }, {
+            label: 'Transaction Total (Credit)',
+            data: [2586, 4139.75, 1692.25, 716],
+            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
+            borderColor: 'rgba(255, 192, 203, 1)', 
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 1000,
+                    callback: function (value) {
+                        return value;
+                    }
+                }
+            }
+        }
+    }
 });
