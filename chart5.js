70d816ea579e6cd9fafc9923f14cfdb401019395
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-22 18:52:00 +0700

    chart5.js

--- chart5.js
+++ chart5.js
@@ -1,32 +1,47 @@
-var xValuesLine = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"];
-var yValuesLine = [884, 909, 1118, 1620, 1589, 1815, 2216, 2041, 1679, 1652, 1532, 1367];
-
-
-new Chart("chart5", {
-type: "line",
-data: {
-    labels: xValuesLine,
-    datasets: [{
-        label: 'Total sales',
-        fill: false,
-        lineTension: 0,
-        backgroundColor: "rgba(165, 42, 42, 1)",
-        borderColor: "rgba(165, 42, 42, 1)",
-        data: yValuesLine
-    }]
-},
-options: {
-    scales: {
-        y: {
-            ticks: {
-                stepSize: 1000,
-                callback: function (value, index, values) {
-                    return value;
+var ctx5 = document.getElementById('chart5').getContext('2d');
+var myChart = new Chart(ctx5, {
+    type: 'bar',
+    data: {
+        labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
+        datasets: [{
+            label: 'Food',
+            data: [2805,3729,1309,1534],
+            backgroundColor: 'rgba(128, 0, 0, 0.5)',
+            borderColor: 'rgba(128, 0, 0, 1)', 
+            borderWidth: 1
+        }, {
+            label: 'Carbonated',
+            data: [3222,992,693,144],
+            backgroundColor: 'rgba(255, 0, 0, 0.5)', 
+            borderColor: 'rgba(255, 0, 0, 1)', 
+            borderWidth: 1
+        }, {
+            label: 'Non Carbonated',
+            data: [878,1115,601,88],
+            backgroundColor: 'rgba(255, 40, 0, 0.5)', 
+            borderColor: 'rgba(255, 40, 0, 1)', 
+            borderWidth: 1
+        }, {
+            label: 'Water',
+            data: [0,551,667,43],
+            backgroundColor: 'rgba(248, 131, 121, 0.5)', 
+            borderColor: 'rgba(248, 131, 121, 1)', 
+            borderWidth: 1
+        }
+    ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 1000,
+                    callback: function (value) {
+                        return value;
+                    }
                 }
             }
         }
     }
-}
 });
-
 
