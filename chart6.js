123beaacc41901cce1fdf4fc8871f06899137131
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-22 18:53:19 +0700

    chart6.js

--- chart6.js
+++ chart6.js
@@ -1,46 +1,33 @@
-var ctx5 = document.getElementById('chart6').getContext('2d');
-var myChart = new Chart(ctx5, {
-    type: 'bar',
-    data: {
-        labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
-        datasets: [{
-            label: 'Food',
-            data: [2805,3729,1309,1534],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgba(255, 99, 132, 1)', 
-            borderWidth: 1
-        }, {
-            label: 'Carbonated',
-            data: [3222,992,693,144],
-            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
-            borderColor: 'rgba(255, 192, 203, 1)', 
-            borderWidth: 1
-        }, {
-            label: 'Non Carbonated',
-            data: [878,1115,601,88],
-            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
-            borderColor: 'rgba(255, 192, 203, 1)', 
-            borderWidth: 1
-        }, {
-            label: 'Water',
-            data: [0,551,667,43],
-            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
-            borderColor: 'rgba(255, 192, 203, 1)', 
-            borderWidth: 1
-        }
-    ]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 1000,
-                    callback: function (value) {
-                        return value;
-                    }
-                }
-            }
-        }
-    }
+const ctx = document.getElementById('chart6');
+const chart6 = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['BSQ Mall x1364 - Zales', 'BSQ Mall x1366 - ATT'],
+    datasets: [{
+      data: [926, 746],
+      label: 'd',
+      data: [55.4, 44.6],
+      backgroundColor: [
+          'rgb(255, 0, 0)',
+          'rgb(255, 99, 125)',
+      ],
+      borderColor: [
+          'rgb(255, 0, 0)',
+          'rgb(255, 99, 125)',
+      ],
+      borderWidth: 1
+  }]
+},
+options: {
+  plugins: {
+      tooltip: {
+          callbacks: {
+              label: function (context) {
+                  let label = context.parsed + '%';
+                  return label;
+              }
+          }
+      }
+  }
+}
 });
