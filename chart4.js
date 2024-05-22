0284fa76fe0b6fc2b538f9c0bf121fd306fd41a2
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-22 18:50:55 +0700

    chart4.js

--- chart4.js
+++ chart4.js
@@ -1,33 +1,30 @@
-var ctx4 = document.getElementById('chart4').getContext('2d');
-var myChart = new Chart(ctx4, {
-    type: 'bar',
-    data: {
-        labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
-        datasets: [{
-            label: 'Transaction Total (Cash)',
-            data: [5210.5, 4139.75, 1845.75, 1220.5],
-            backgroundColor: '(165, 42, 42, 1)', 
-            borderColor: 'rgba(165, 42, 42, 1)', 
-            borderWidth: 1
-        }, {
-            label: 'Transaction Total (Credit)',
-            data: [2586, 3100.75, 1692.25, 716],
-            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
-            borderColor: 'rgba(255, 192, 203, 1)', 
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 1000,
-                    callback: function (value) {
-                        return value;
-                    }
+var xValuesLine = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"];
+var yValuesLine = [884, 909, 1118, 1620, 1589, 1815, 2216, 2041, 1679, 1652, 1532, 1367];
+
+
+new Chart("chart4", {
+type: "line",
+data: {
+    labels: xValuesLine,
+    datasets: [{
+        label: 'Total Sales',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(165, 42, 42, 1)",
+        borderColor: "rgba(165, 42, 42, 1)",
+        data: yValuesLine
+    }]
+},
+options: {
+    scales: {
+        y: {
+            ticks: {
+                stepSize: 1000,
+                callback: function (value, index, values) {
+                    return value;
                 }
             }
         }
     }
+}
 });
