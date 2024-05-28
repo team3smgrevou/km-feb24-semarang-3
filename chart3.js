88b4590396b1e7e6531ef604689dddcf6cc5c0a6
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-28 17:24:56 +0700

    chart3.js

--- chart3.js
+++ chart3.js
@@ -1,19 +1,27 @@
+import typeByLocation from './typeByLocation.json' assert {type : 'json'}
+
+console.log(typeByLocation)
+
+const locations = typeByLocation.map(entry => entry.location);
+const totalCash = typeByLocation.map(entry => parseFloat(entry.total_cash));
+const totalCredit = typeByLocation.map(entry => parseFloat(entry.total_credit));
+
 var ctx3 = document.getElementById('chart3').getContext('2d');
 var myChart = new Chart(ctx3, {
     type: 'bar',
     data: {
-        labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
+        labels: locations,
         datasets: [{
             label: 'Transaction Total (Cash)',
-            data: [5210.5, 3100.75, 1845.75, 1220.5],
-            backgroundColor: 'rgba(165, 42, 42, 1)', 
-            borderColor: 'rgba(165, 42, 42, 1)', 
+            data: totalCash,
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            borderColor: 'rgba(255, 0, 0, 1)',  
             borderWidth: 1
         }, {
             label: 'Transaction Total (Credit)',
-            data: [2586, 4139.75, 1692.25, 716],
-            backgroundColor: 'rgba(255, 192, 203, 0.5)', 
-            borderColor: 'rgba(255, 192, 203, 1)', 
+            data: totalCredit,
+            backgroundColor: 'rgba(248, 131, 121, 0.5)', 
+            borderColor: 'rgba(248, 131, 121, 1)',  
             borderWidth: 1
         }]
     },
