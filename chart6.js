135fb62558a744acf3b1f19555397cdd3f05641e
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-28 17:30:15 +0700

    chart6.js

--- chart6.js
+++ chart6.js
@@ -1,19 +1,24 @@
-const ctx = document.getElementById('chart6');
+import salesInBsqMall from './salesInBsqMall.json' assert {type: 'json'};
+
+console.log(salesInBsqMall);
+
+const labels = salesInBsqMall.map(entry => 'BSQ Mall x1364 - Zales - BSQ Mall x1366 - ATT');
+const data = salesInBsqMall.map(entry => parseInt(entry.total_sales));
+
+const ctx = document.getElementById('chart6').getContext('2d');
 const chart6 = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ['BSQ Mall x1364 - Zales', 'BSQ Mall x1366 - ATT'],
+    labels: labels,
     datasets: [{
-      data: [926, 746],
-      label: 'd',
-      data: [55.4, 44.6],
+      data: data,
       backgroundColor: [
-          'rgb(255, 0, 0)',
-          'rgb(255, 99, 125)',
+        'rgba(255, 40, 0, 0.5)', 
+        'rgba(248, 131, 121, 0.5)',  
       ],
       borderColor: [
-          'rgb(255, 0, 0)',
-          'rgb(255, 99, 125)',
+        'rgba(255, 40, 0, 1)', 
+        'rgba(248, 131, 121, 1)',
       ],
       borderWidth: 1
   }]
