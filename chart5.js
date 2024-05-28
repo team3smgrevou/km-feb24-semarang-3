3f18177f9ab6238c2f0bb2079b6cc0d767e2e20b
Mukti Anugrah Pratama <140960597+Mukti047@users.noreply.github.com>
2024-05-28 17:28:11 +0700

    chart5.js

--- chart5.js
+++ chart5.js
@@ -1,29 +1,61 @@
+import salesCategoryByLocation from './salesCategoryByLocation.json' assert {type : 'json'}
+
+console.log(salesCategoryByLocation)
+
+var dataJSON = salesCategoryByLocation
+
+var food =[]
+var carbonated = []
+var nonCarbonated = []
+var water = []
+
+
+for (var i=0;i<salesCategoryByLocation.length;i++){
+    
+    if (salesCategoryByLocation[i].category == "Food"){
+        food = [salesCategoryByLocation[i].GuttenPlans , salesCategoryByLocation[i].EB_Public_Library , salesCategoryByLocation[i].Brunswick_Sq_Mall , salesCategoryByLocation[i].Earle_Asphalt]
+    }
+
+    if (salesCategoryByLocation[i].category == "Carbonated"){
+        carbonated = [salesCategoryByLocation[i].GuttenPlans , salesCategoryByLocation[i].EB_Public_Library , salesCategoryByLocation[i].Brunswick_Sq_Mall , salesCategoryByLocation[i].Earle_Asphalt]
+    }
+
+    if (salesCategoryByLocation[i].category == "Non Carbonated"){
+        nonCarbonated = [salesCategoryByLocation[i].GuttenPlans , salesCategoryByLocation[i].EB_Public_Library , salesCategoryByLocation[i].Brunswick_Sq_Mall , salesCategoryByLocation[i].Earle_Asphalt]
+    }
+
+    if (salesCategoryByLocation[i].category == "Water"){
+        water = [salesCategoryByLocation[i].GuttenPlans , salesCategoryByLocation[i].EB_Public_Library , salesCategoryByLocation[i].Brunswick_Sq_Mall , salesCategoryByLocation[i].Earle_Asphalt]
+    }
+}
+
 var ctx5 = document.getElementById('chart5').getContext('2d');
 var myChart = new Chart(ctx5, {
     type: 'bar',
     data: {
         labels: ['GuttenPlans', 'EB Public Library','Brunswick Sq Mall','Earle Asphalt',],
-        datasets: [{
+        datasets: [
+            {
             label: 'Food',
-            data: [2805,3729,1309,1534],
+            data: food,
             backgroundColor: 'rgba(128, 0, 0, 0.5)',
             borderColor: 'rgba(128, 0, 0, 1)', 
             borderWidth: 1
         }, {
             label: 'Carbonated',
-            data: [3222,992,693,144],
+            data: carbonated,
             backgroundColor: 'rgba(255, 0, 0, 0.5)', 
             borderColor: 'rgba(255, 0, 0, 1)', 
             borderWidth: 1
         }, {
             label: 'Non Carbonated',
-            data: [878,1115,601,88],
+            data: nonCarbonated,
             backgroundColor: 'rgba(255, 40, 0, 0.5)', 
             borderColor: 'rgba(255, 40, 0, 1)', 
             borderWidth: 1
         }, {
             label: 'Water',
-            data: [0,551,667,43],
+            data: water,
             backgroundColor: 'rgba(248, 131, 121, 0.5)', 
             borderColor: 'rgba(248, 131, 121, 1)', 
             borderWidth: 1
@@ -44,4 +76,3 @@ var myChart = new Chart(ctx5, {
         }
     }
 });
-
